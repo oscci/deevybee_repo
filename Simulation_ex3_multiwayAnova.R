c80ad7cb9d60ee9a07db4df794184fe7f8a3fdef
Dorothy Bishop <dorothy.bishop@psy.ox.ac.uk>
2017-04-28 06:24:42 +0100

    removed tidy_eval at end of file

--- Simulation_ex3_multiwayAnova.R
+++ Simulation_ex3_multiwayAnova.R
@@ -115,6 +115,3 @@ paste('% ANOVAs with one effect or interaction < .007 = ',percentBon)
   # This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. https://creativecommons.org/licenses/by-nc-sa/4.0/
 #   
 
-tidy_eval(comment=FALSE)
-  
-#   
